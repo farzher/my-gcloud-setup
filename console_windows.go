4211d13d96bd44f2d58fdec96e2977c9f3380788
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-15 00:23:26 -0400

    Flush Windows input before interactive SSH

--- console_windows.go
+++ console_windows.go
@@ -2,14 +2,25 @@
 
 package main
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+)
 
 const consoleUTF8 = 65001
 
+var (
+	kernel32                = syscall.NewLazyDLL("kernel32.dll")
+	setConsoleCP            = kernel32.NewProc("SetConsoleCP")
+	setConsoleOutputCP      = kernel32.NewProc("SetConsoleOutputCP")
+	flushConsoleInputBuffer = kernel32.NewProc("FlushConsoleInputBuffer")
+)
+
 func configureConsole() {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	setConsoleCP := kernel32.NewProc("SetConsoleCP")
-	setConsoleOutputCP := kernel32.NewProc("SetConsoleOutputCP")
 	_, _, _ = setConsoleCP.Call(consoleUTF8)
 	_, _, _ = setConsoleOutputCP.Call(consoleUTF8)
+}
+
+func flushConsoleInput() {
+	_, _, _ = flushConsoleInputBuffer.Call(os.Stdin.Fd())
 }
